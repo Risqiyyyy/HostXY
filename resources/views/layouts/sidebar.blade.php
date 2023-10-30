07bd43d1169e8e7ceedbb0df03323b355263acc2
risqiyyyy <raisafebriani05@gmail.com>
2023-10-30 11:31:45 +0700

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,31 @@
+<div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+    <div class="nano">
+        <div class="nano-content">
+            <ul>
+                <div class="logo"><a href="index.html">
+                        <!-- <img src="images/logo.png" alt="" /> --><span>Focus</span></a></div>
+                <li class="label">Main</li>
+                <li><a href="{{route('dashboard')}}"><i class="ti-home"></i>Dashboard</a></li>
+                <li><a class="sidebar-sub-toggle"><i class="ti-bar-chart-alt"></i>Layanan Dan Paket <span
+                            class="sidebar-collapse-icon ti-angle-down"></span></a>
+                    <ul>
+                        <li><a href="chart-flot.html">Hosting</a></li>
+                        <li><a href="chart-morris.html">Domain</a></li>
+                    </ul>
+                </li>
+                <hr>
+                <li><a href="app-event-calender.html"><i class="ti-calendar"></i> Akses Cpanel </a></li>
+                <li><a href="app-email.html"><i class="ti-email"></i> Database</a></li>
+                <li><a href="app-profile.html"><i class="ti-user"></i> File Manager</a></li>
+                <li><a class="sidebar-sub-toggle"><i class="ti-bar-chart-alt"></i>Domain <span
+                    class="sidebar-collapse-icon ti-angle-down"></span></a>
+                    <ul>
+                        <li><a href="chart-flot.html">Addons Domain</a></li>
+                        <li><a href="chart-morris.html">Sub Domain</a></li>
+                    </ul>
+                </li>
+                <li><a href="app-widget-card.html"><i class="ti-layout-grid2-alt"></i>Costumer Service</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
